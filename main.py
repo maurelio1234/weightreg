02532b24759a65eb8e165a83337b417ddfd4a355
marcos <maurelio1234@gmail.com>
2015-01-14 06:47:50 +0000

    removing test mode switch

--- main.py
+++ main.py
@@ -31,10 +31,6 @@ def weight_changed_action(sender):
 
 def height_changed_action(sender):
 	model.set_height(main_view['textfield_height'].text)
-	
-def switch_test_mode_action(sender):
-	global main_view
-	model.config.test_mode = main_view['switch_test_mode'].value
 
 main_view = ui.load_view('main')
 height = model.get_height()
@@ -42,7 +38,6 @@ height = model.get_height()
 main_view['textfield_height'].text = str(height)
 main_view['textfield_height'].action = height_changed_action
 main_view['textfield_weight'].action = weight_changed_action
-main_view['switch_test_mode'].value = model.config.test_mode # TODO remove this switch
 main_view['textfield_ideal_weight'].text = '{:.1f}'.format(model.ideal_weight(height))
 
 main_view['trend_15_days'].text = '{:.1f} Kg'.format(model.estimate_weight(datetime.now() - timedelta(weeks=2)))
@@ -59,6 +54,7 @@ class PlotThread(threading.Thread):
 		
 		status.hidden = False
 		picture.hidden = True
+		
 		status.text = 'Please wait, generating plot...'
 		data = model.generate_plot()
 		picture.image = ui.Image.from_data(data)
